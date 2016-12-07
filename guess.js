537da6bf94698ed41cf1d6d03583f7d8e46b6350
Rebecca Holt <rebecca.holt9991@gmail.com>
2016-12-07 13:27:47 -0700

    Made a function for clearing all fields to call in other functions

--- guess.js
+++ guess.js
@@ -9,19 +9,26 @@ var guessedNumber = document.getElementById("number-guesser");
 var clearFieldButton = document.querySelector(".clear-field");
 var resetButton = document.querySelector(".reset-button");
 
+// Submit Button Event Listener
 guessSubmit.addEventListener('click', function() {
   lastGuess.innerText = guessedNumber.value;
   compareGuess();
   alertMessage();
  });
 
+// Empty All fields function
+ function empltyAllFields(){
+   guessedNumber.value = "";
+   lastGuess.innerText = "";
+   tooHighLow.innerText = "";
+ };
 
+// Boom, too high, too low function
 function compareGuess () {
   var guessed = parseInt(guessedNumber.value);
 
   if (guessed === randomNumber) {
     tooHighLow.innerText = "BOOM!";
-    tooHighLow.innerHTML = "";
   } else if (guessed > randomNumber) {
     tooHighLow.innerText = "Too high! Try again!";
   } else if (guessed < randomNumber) {
@@ -29,30 +36,30 @@ function compareGuess () {
   }
 };
 
+// Clear button event listener
 clearFieldButton.addEventListener('click', function() {
   guessedNumber.value = "";
  });
 
-
+//Reset Button event listener
  resetButton.addEventListener('click' , function() {
-   guessedNumber.value = "";
-   lastGuess.innerText = "";
-   tooHighLow.innerText = "";
+   empltyAllFields();
    randomNumber = randoNumber();
  });
 
-
+// Create a new random number function
 function randoNumber() {
   var randomNumber = Math.floor(Math.random() * 100 + 1)
   return randomNumber;
 };
 
-
 function alertMessage () {
   var guessed = parseInt(guessedNumber.value);
   if (guessed < 0) {
     alert ("ERROR: Enter a number between 0-100");
+    empltyAllFields();
   } else if (guessed > 100) {
     alert ("ERROR: Enter a number between 0-100");
+    empltyAllFields();
   }
 };
